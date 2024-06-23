4c0d647ce5e7a5d2d9b782832d91d323f539761a
aviv <63423203+aviv2001@users.noreply.github.com>
2024-06-23 12:37:17 +0300

    עדכון מפתח
    עדכון מפתח

--- itim_react_app/src/LandingPage/Map.js
+++ itim_react_app/src/LandingPage/Map.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const GOOGLE_MAPS_API_KEY = 'AIzaSyCvIJYnnAZrhhIpP-LaizRToTInrU6K8fc'; // replace with your actual API key
+const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API; // replace with your actual API key
 
 const defaultCenter = {
     lat: 31.7683,
     lng: 35.2137, // Jerusalem coordinates
 };
-
 const Map = () => {
     const [location, setLocation] = useState(null);
     const [isLocationError, setIsLocationError] = useState(false);
