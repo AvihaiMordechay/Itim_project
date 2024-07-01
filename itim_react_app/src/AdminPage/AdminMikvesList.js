f5a176d3ba11595012d01fbfd18d4c2f627cff2a
EladOzery <eladozery2@gmail.com>
2024-07-01 20:32:01 +0300

    הוספת עמודות תברואה לחיפוש אדמין

--- itim_react_app/src/AdminPage/AdminMikvesList.js
+++ itim_react_app/src/AdminPage/AdminMikvesList.js
@@ -5,6 +5,35 @@ const AdminMikvesList = ({ presentationMikves, handleEditMikve, visibleCount, se
     const handleShowMore = () => {
         setVisibleCount((prevCount) => prevCount + numOfRows);
     };
+
+    // Function to change 0/1/2 values to its respected strings
+    const getWaterSamplingStatus = (value) => {
+        switch (value) {
+            case "0":
+                return "לא נבדק";
+            case "1":
+                return "נבדק ותקין";
+            case "2":
+                return "נבדק ולא תקין";
+            default:
+                return "-";
+        }
+    };
+
+    // Function to format a date to DD.MM.YYYY
+    const formatDate = (dateStr) => {
+        const date = new Date(dateStr);
+        if (isNaN(date.getTime())) {
+            // If date is invalid, return the original string
+            return dateStr;
+        }
+        // Extract day, month, and year
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+        const year = date.getFullYear();
+        return `${day}.${month}.${year}`;
+    };
+
     return (
         <div className="admin-mikves-list">
             <table>
@@ -15,6 +44,8 @@ const AdminMikvesList = ({ presentationMikves, handleEditMikve, visibleCount, se
                         <th>עיר</th>
                         <th>כתובת</th>
                         <th>טלפון</th>
+                        <th>תברואה</th>
+                        <th>תאריך בדיקת תברואה</th>
                         <th>הערות</th>
                         <th>עריכה</th>
                     </tr>
@@ -28,6 +59,8 @@ const AdminMikvesList = ({ presentationMikves, handleEditMikve, visibleCount, se
                                 <td>{mikve.city}</td>
                                 <td>{mikve.address}</td>
                                 <td>{mikve.phone}</td>
+                                <td>{getWaterSamplingStatus(mikve.water_sampling)}</td>
+                                <td>{formatDate(mikve.when_sampling)}</td>
                                 <td>{mikve.notes}</td>
                                 <td>
                                     <button className="admin-mikve-edit-button"
