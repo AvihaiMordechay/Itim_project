b4c7dbff69a36ded7f70159b0b607db0e05e5746
aviv <63423203+aviv2001@users.noreply.github.com>
2024-07-16 18:50:36 +0300

    עדכוני עיצוב הדר וסטטיסטיקות

--- itim_react_app/src/AdminPage/AdminHeader.js
+++ itim_react_app/src/AdminPage/AdminHeader.js
@@ -12,14 +12,22 @@ const AdminHeader = ({ allMikves, setAllMikves, handleUploadSuccess }) => {
             <h1>ניהול מקוואות</h1>
             <div className="admin-operations">
                 <h3>פעולות:</h3>
-                <AdminAddMikve />
-                <AdminDownloadData allMikves={allMikves} />
-                <AdminUploadSamplingXL
-                    allMikves={allMikves}
-                    setAllMikves={setAllMikves}
-                    onUploadSuccess={handleUploadSuccess}
-                />
-                <AdminStatistics allMikves={allMikves} />
+                <div className="operations-button">
+                    <AdminAddMikve />
+                </div>
+                <div className="operations-button">
+                    <AdminDownloadData allMikves={allMikves} />
+                </div>
+                <div className="operations-button">
+                    <AdminUploadSamplingXL
+                        allMikves={allMikves}
+                        setAllMikves={setAllMikves}
+                        onUploadSuccess={handleUploadSuccess}
+                    />
+                </div>
+                <div className="operations-button">
+                    <AdminStatistics allMikves={allMikves} />
+                </div>
             </div>
         </div>
     );
